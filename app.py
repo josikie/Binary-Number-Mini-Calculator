eacee942f3e66b7206a31f8ebf17019380c4cc55
Josi Kie <kiejosi12@gmail.com>
2023-01-21 18:06:22 +0700

    Added terms in addBinaryNumbers function

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def lengthLongerThan(lenA, lenB, sA, sB):
     
     return sB
 
+
 def addBinaryNumbers(stringOne, stringTwo):
     # take the length
     sOneLength = len(stringOne)
@@ -45,10 +46,47 @@ def addBinaryNumbers(stringOne, stringTwo):
     if sTwoLength > sOneLength:
         stringOne = lengthLongerThan(sTwoLength, sOneLength, stringTwo, stringOne)
     # add binary numbers.
+    sOneSTwo = ""
+    temp = '0'
+    for i in range(sOneLength, -1):
+        number = '0'
+        if stringOne[i] == '1' and stringTwo[i] == '1':
+            if temp == '1' and i == 0:
+                number = '1'
+                sOneSTwo += number
+                number = '1'
+                temp = '0'
+            elif temp == '0' and i == 0:
+                number = '0'
+                sOneSTwo += number
+                number = "1"
+                temp = "0"
+            elif temp == '1':
+                number = '1'
+                temp = '1'
+            elif temp == '0':
+                number = '0'
+                temp = '1'
+        elif stringOne == '1' and stringTwo[i] == '0':
+            if temp == '1' and i == 0:
+                number = '0'
+                sOneSTwo += number
+                number = '1'
+                temp = '0'
+            elif temp == '0' and i == 0:
+                number = '1'
+                temp = '0'
+            elif temp == '1':
+                number = '0'
+                temp = '1'
+            elif temp == '0':
+                number = '1'
+                temp = '0'
+                
     return {
         'stringOne' : stringOne,
         'stringTwo' : stringTwo
-    };
+    }
 
 print('Masukkan dua string binary number: ')
 stringOne = input()
